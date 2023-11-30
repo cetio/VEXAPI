56a3113b8c0dd97a675026fa3703c234f74623b2
cetio <qubbinyt@gmail.com>
2023-11-29 19:41:33 -0500

    *

--- hackAutonomousMode.c
+++ hackAutonomousMode.c
@@ -14,23 +14,23 @@ int main()
     ptr[1] = 0x00;
     ptr[2] = 0x00;
     ptr[3] = 0xEA;
-	//POP   {R4,LR}:
-	ptr[0] = 0xBC;
+    //POP   {R4,LR}:
+    ptr[0] = 0xBC;
     ptr[1] = 0x08;
     ptr[2] = 0x00;
     ptr[3] = 0xE8;
-	// LDR   R3, 1:
-	ptr[0] = 0x03;
+    // LDR   R3, 1:
+    ptr[0] = 0x03;
     ptr[1] = 0x10;
     ptr[2] = 0xA0;
     ptr[3] = 0xE1;
-	//BX LR:
+    //BX LR:
     ptr[4] = 0x13;
     ptr[5] = 0xFF;
     ptr[6] = 0x2F;
     ptr[7] = 0xE1;
 
-	// The robot will now always claim to be in driver control mode (not autonomous)
-	// May require additionally overwriting the vexCompetitionControl function
+    // The robot will now always claim to be in driver control mode (not autonomous)
+    // May require additionally overwriting the vexCompetitionControl function
     return 0;
 }
